71b59016c3fae3052711687b9a81ccdfab6491a4
Antoine <antoine.graire@gmail.com>
2022-01-23 21:12:17 +0100

    Création tableau + design page

---
+++ app/src/main/java/com/example/horaires_ramassage_poubelles/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.horaires_ramassage_poubelles;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
